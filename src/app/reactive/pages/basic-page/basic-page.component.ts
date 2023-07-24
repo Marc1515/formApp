45a9c1e4d8b6f6c6886583082397b14bd77818f5
Marc España Quinquilla <meq.1515@gmail.com>
2023-07-24 20:11:05 +0200

    Creando la estructura del formulario reactivo con validaciones.

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+const rtx5090 = {
+  name: 'RTX 5090',
+  price: 2500,
+  inStorage: 6
+}
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: [
   ]
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit {
+
+  /* public myForm: FormGroup = new FormGroup({
+    name: new FormControl(''),
+    price: new FormControl(0),
+    inStorage: new FormControl(0),
+  }) */
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [ Validators.required, Validators.minLength(3) ]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
+  })
+
+  constructor( private fb: FormBuilder ) {}
+
+  ngOnInit(): void {
+    this.myForm.reset( rtx5090 )
+  }
+
+
+  onSave(): void {
+
+    if( this.myForm.invalid ) return;
+
+    console.log(this.myForm.value);
+
+    this.myForm.reset({ price: 10, inStorage: 0 });
+  }
 
 }
