713f1d0e158b651b5021570d7e2b202b02c2bf3c
Marc España Quinquilla <meq.1515@gmail.com>
2023-07-25 17:27:44 +0200

    Hacemos uso de las diferentes formas de usar el Validators con el archivo creado explicado en el anterior commit.

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as customValidators from 'src/app/shared/validators/validators';
 
 @Component({
   selector: 'app-register-page',
@@ -7,5 +9,27 @@ import { Component } from '@angular/core';
   ]
 })
 export class RegisterPageComponent {
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [ Validators.required, Validators.pattern(customValidators.firstNameAndLastnamePattern) ]],
+    email: ['', [ Validators.required, Validators.pattern(customValidators.emailPattern) ]],
+    username: ['', [ Validators.required, customValidators.cantBeStrider ]],
+    password: ['', [ Validators.required, Validators.minLength(6) ]],
+    password2: ['', [ Validators.required ]],
+  })
+
+
+  constructor( private fb:FormBuilder ) {}
+
+  isValidField( field: string ) {
+    // TODO: obtener validación desde un servicio
+  }
+
+  onSubmit() {
+    if ( this.myForm.invalid ) {
+      this.myForm.markAllAsTouched();
+    }
+    console.log(this.myForm.value)
+  }
 
 }
